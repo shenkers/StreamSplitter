fcff50fde232c46508554a122b9a352949d00b47
Sol Shenker <sol.shenker@gmail.com>
2024-12-08 21:01:06 -0500

    fix test

--- src/test/groovy/shenkerbfx/StreamSplitter/ApplicationSpec.groovy
+++ src/test/groovy/shenkerbfx/StreamSplitter/ApplicationSpec.groovy
@@ -3,12 +3,6 @@ package shenkerbfx.StreamSplitter
 import spock.lang.Specification
 
 class ApplicationSpec extends Specification {
-
-    def "this should fail" () {
-        expect:
-        2 == 1
-    }
-
     def "this should pass" () {
         expect:
         1 == 1
